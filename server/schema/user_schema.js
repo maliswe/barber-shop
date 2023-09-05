bc89d1f00774d0b42734dc6cff0079bf75b33412
Aslan <muhmmad@student.chalmers.se>
2023-09-05 15:31:59 +0200

    added admin controller

--- server/schema/user_schema.js
+++ server/schema/user_schema.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    phone: { type: Number, required: true },
-    email: { type: String, required: true },
+    phone: { type: Number, required: true, unique: true},
+    email: { type: String, required: true, unique: true},
     password: { type: String, required: true } 
 }, {
     role: {
