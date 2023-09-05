ac7cb4ddb7671de10e90eefc9fdb4bc3816c807a
muhmmad <muhmmad@student.chalmers.se>
2023-09-05 14:12:54 +0200

    added controller

---
+++ server/controller/admin_controller.js
@@ -0,0 +1,40 @@
+const Admin = require('../schema/admin_schema.js')
+
+
+
+const getAll = async (req, res) => {
+    try {
+        // Use Mongoose to query the MongoDB database for admin data
+        const admins = await Admin.find(); // This fetches all admin documents in the 'admins' collection
+
+        // Send the data as a response to the client
+        res.json(admins);
+    } catch (error) {
+        // Handle any errors
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+const create = async (req, res) => {
+    try {
+        // Create a new admin document based on the request body
+        const newAdmin = new Admin(req.body);
+
+        // Save the new admin document to the database
+        const savedAdmin = await newAdmin.save();
+
+        // Send the saved admin data as a response
+        res.status(201).json(savedAdmin); // 201 Created status code
+    } catch (error) {
+        // Handle any errors
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+
+module.exports = {
+    getAll,
+    create
+};
--- server/routes/admin_routes.js
+++ server/routes/admin_routes.js
@@ -1,37 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Admin = require('../schema/admin_schema.js')
+const controller = require('../controller/admin_controller.js')
 
 router.get('/', async (req, res) => {
-    try {
-        // Use Mongoose to query the MongoDB database for admin data
-        const admins = await Admin.find(); // This fetches all admin documents in the 'admins' collection
-
-        // Send the data as a response to the client
-        res.json(admins);
-    } catch (error) {
-        // Handle any errors
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    controller.getAll(req, res);
 });
 
 
 router.post('/', async (req, res) => {
-    try {
-        // Create a new admin document based on the request body
-        const newAdmin = new Admin(req.body);
-
-        // Save the new admin document to the database
-        const savedAdmin = await newAdmin.save();
-
-        // Send the saved admin data as a response
-        res.status(201).json(savedAdmin); // 201 Created status code
-    } catch (error) {
-        // Handle any errors
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    controller.create(req, res);
 });
 
 
