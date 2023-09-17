8bf7c161deea5ceb40da106caf8700e73a25974b
yousefab <yousefab@net.chalmers.se>
2023-09-17 23:45:54 +0200

    added slideshow

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
     <Navbar />
+    <carousel />
     <b-jumbotron header="Collection hair & beard" lead="Welcome to Old School BarberShop">
         <ServicesButton />
     </b-jumbotron>
@@ -10,12 +11,14 @@
 <script>
 import Navbar from '../component/Nav/Navbar.vue'
 import ServicesButton from '../component/Home/ServicesButton.vue'
+import carousel from '../component/Home/carousel.vue'
 
 export default {
   name: 'home',
   components: {
     Navbar,
-    ServicesButton
+    ServicesButton,
+    carousel
   }
 }
 </script>
