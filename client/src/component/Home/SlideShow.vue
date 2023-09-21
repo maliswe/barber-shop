077ebd2ce7aa3c829136f19ba3e54a6abbdbf2bc
yousefab <yousefab@net.chalmers.se>
2023-09-21 17:30:52 +0200

    Added images Best Services

--- client/src/component/Home/SlideShow.vue
+++ client/src/component/Home/SlideShow.vue
@@ -3,10 +3,23 @@
     <div class="header">
       <h1>Our Best Services</h1>
     </div>
-    <div class="slideshow">
-      <img :src="currentImage" alt="Slideshow" />
-      <button @click="prevSlide" class="slideshow-button prev">Previous</button>
-      <button @click="nextSlide" class="slideshow-button next">Next</button>
+    <div class="image-container">
+      <div
+        v-for="(image, index) in images"
+        :key="index"
+        :class="['image-item', 'image-' + (index + 1), { 'selected': index === currentIndex }]"
+        @click="setCurrentIndex(index)"
+        :style="imagePositions[index]"
+      >
+        <img
+          :src="require(`@/component/images/${image}`)"
+          :alt="`Image ${index + 1}`"
+          class="resizable-image"
+        />
+        <div class="overlay" v-if="index === currentIndex"></div>
+        <div class="thumbnail-buttons">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,63 +28,70 @@
 export default {
   data() {
     return {
-      images: [
-        'first.png',
-        'second.png',
-        'third.png'
-      ],
-      currentIndex: 0
-    }
-  },
-  computed: {
-    currentImage() {
-      return `/component/images/${this.images[this.currentIndex]}`
+      images: ['first.png', 'second.png', 'third.png', 'forth.png'],
+      currentIndex: 0,
+      imagePositions: [
+        { left: '320px', bottom: '-1400px' },
+        { left: '640px', bottom: '-1400px' },
+        { left: '960px', bottom: '-1400px' },
+        { left: '1280px', bottom: '-1400px' } // Adjust positions as needed
+      ]
     }
   },
   methods: {
-    nextSlide() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length
-    },
-    prevSlide() {
-      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length
+    setCurrentIndex(index) {
+      this.currentIndex = index
     }
   }
 }
 </script>
 
-<style scoped>
-.header {
-  position: relative;
-  padding: 2px;
-}
-
-.slideshow {
+<style>
+.image-item {
+  text-align: center;
+  padding: 20px;
+  cursor: pointer;
+  position: absolute;
   width: 300px;
-  overflow: hidden;
-  position: relative;
+  height: 400px;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Dark overlay color */
+  display: none;
+  transition: opacity 0.3s;
 }
 
-.slideshow img {
+.selected .overlay {
+  display: block;
+}
+
+.image-1 {
+  left: 320px;
+  bottom: '-1400px';
+}
+
+.image-2 {
+  left: 640px;
+  bottom: '-1400px';
+}
+
+.image-3 {
+  left: 960px;
+  bottom: '-1400px';
+}
+
+.image-4 {
+  left: 1280px;
+  bottom: '-1400px';
+}
+
+.resizable-image {
   width: 100%;
   height: auto;
-}
-
-.slideshow-button {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-}
-
-.slideshow-button.prev {
-  left: 0;
-}
-
-.slideshow-button.next {
-  right: 0;
 }
 </style>
