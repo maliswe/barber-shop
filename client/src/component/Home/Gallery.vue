9335748f29ba53f4a41460f66f3321d26aae19c0
yosuefab <yousefab@net.chalmers.se>
2023-09-22 17:41:48 +0200

    gallery salon added

---
+++ client/src/component/Home/Gallery.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h1-text">
+    <h1>Salon Gallery</h1>
+  </div>
+</template>
+
+<style>
+.h1-text {
+  position: absolute;
+  bottom: 70px;
+  left: 296px;
+  right: 0;
+  color: #000000;
+  padding: 20px;
+}
+</style>
