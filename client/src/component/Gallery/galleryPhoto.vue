649e85c63aac7d40d5de87320fbd0b543e1ef84a
Aslan <muhmmad@student.chalmers.se>
2023-10-02 05:32:57 +0200

    coneccted Gallery page

---
+++ client/src/component/Gallery/galleryPhoto.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header-photo">
+      <img src='@/assets/about.png' alt="Image of a man cutting a client hair"/>
+    </div>
+</template>
+
+<script>
+const imageLink = '@/assets/about.png'
+export default {
+  data() {
+    return {
+      imageLink: imageLink
+    }
+  }
+}
+</script>
