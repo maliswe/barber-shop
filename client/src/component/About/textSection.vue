eac887b3def2b6eba1a6f6d79b5830a5da79ef3d
Aslan <muhmmad@student.chalmers.se>
2023-10-10 23:53:56 +0200

    admin can edit text in about page

--- client/src/component/About/textSection.vue
+++ client/src/component/About/textSection.vue
@@ -1,22 +1,19 @@
 <template>
     <div class="text">
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
-            maecenas accumsan lacus vel facilisis Quis ipsum suspendisse ultrices gravida Risus commodo viverra
-            send do eiusmod  maecenas accumsan lacus vel facilisis.<br><br>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrice
-            QuIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrice
-            Quis ipsum suspendisse ultrices gravida Risus commodo viverra
-            send do eiusmod  maecenas accumsan lacus vel facilisis.is ipsum suspendisse ultrices gravida Risus commodo viverra
-            send do eiusmod  maecenas accumsan lacus vel facilisis.<br><br>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrice
-            QuIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrice
-            Quis ipsum suspendisse ultrices gravida Risus commodo viverra
-            send do eiusmod  maecenas accumsan lacus vel facilisis.is ipsum suspendisse ultrices gravida Risus commodo viverra
-            send do eiusmod  maecenas accumsan lacus vel facilisis.
-        </p>
+      <p>{{ text }}</p>
     </div>
 </template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
 
 <style>
 .text {
