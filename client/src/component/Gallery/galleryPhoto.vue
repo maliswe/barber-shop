154944027d5cdb353ed5e8b68119ea80885ac896
Aslan <muhmmad@student.chalmers.se>
2023-10-10 21:48:20 +0200

    added gallery page

--- client/src/component/Gallery/galleryPhoto.vue
+++ client/src/component/Gallery/galleryPhoto.vue
@@ -1,16 +1,82 @@
 <template>
-    <div class="header-photo">
-      <img src='@/assets/about.png' alt="Image of a man cutting a client hair"/>
+  <div class="gallery" @click="handleImageClick">
+    <div v-if="false" class="delete-button" @click="handleDelete">
+      <i class="fas fa-times-circle"></i>
     </div>
+    <img :src="getImageUrl(gallery.image, gallery.name, gallery.description)" alt="Gallery Image" />
+  </div>
 </template>
 
 <script>
-const imageLink = '@/assets/about.png'
+import { galleries } from '@/api/galleryApi'
 export default {
+  props: {
+    gallery: {
+      type: Object,
+      required: true
+    }
+  },
   data() {
     return {
-      imageLink: imageLink
+      imageURL: '',
+      imageName: '',
+      imageDescription: ''
+    }
+  },
+
+  methods: {
+    getImageUrl(imageBufferObject, imageName, imageDescription) {
+      if (!imageBufferObject || !imageBufferObject.data || !Array.isArray(imageBufferObject.data)) {
+        return null
+      }
+
+      const blob = new Blob([new Uint8Array(imageBufferObject.data)], { type: imageBufferObject.type })
+      const dataUrl = URL.createObjectURL(blob)
+      this.imageURL = dataUrl
+      this.imageName = imageName
+      this.imageDescription = imageDescription
+      return dataUrl
+    },
+    async handleDelete() {
+      const confirmation = window.confirm('Do you really want to delete?')
+      if (confirmation) {
+        try {
+          await galleries.deleteGallery(this.gallery._id)
+          this.$emit('gallery-deleted', this.gallery._id)
+        } catch (error) {
+          console.error('Error fetching galleries:', error)
+        }
+      }
+    },
+    async handleEdit() {
+      this.$emit('edit-gallery', this.gallery)
+    },
+    async handleImageClick() {
+      this.$emit('click', this.imageURL, this.imageName, this.imageDescription)
     }
   }
 }
 </script>
+
+<style scoped>
+.gallery {
+    display: flex;
+    flex-direction: row;
+}
+
+.gallery img {
+  max-width: fill;
+  cursor: pointer;
+}
+
+.delete-button i {
+  color: rgb(255, 136, 0);
+  transition: color 0.3s ease-in-out;
+}
+
+.delete-button:hover i{
+  color: red;
+  font-size: 30px;
+}
+
+</style>
--- client/src/views/Gallery.vue
+++ client/src/views/Gallery.vue
@@ -1,17 +1,98 @@
 <template>
-    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-        <headerPhoto  class="w-100 shadow-1-strong rounded mb-4"/>
-    </div>
+    <div class="gallery-container">
+      <div class="gallery-row">
+        <headerPhoto class="gallery-img"
+        v-for="gallery in galleries"
+        :key="gallery._id"
+        :gallery="gallery"
+        @gallery-deleted="handleGalleryDeleted"
+        @edit-gallery="handleEditGallery"
+        @click="openPopup"
+        />
+      </div>
+      <Popup
+      :show="showModel"
+      :image="popupImage"
+      @close="closeModal"
+      />
+  </div>
 </template>
 
 <script>
 
 import headerPhoto from '../component/Gallery/galleryPhoto.vue'
+import Popup from '../component/Gallery/popUp.vue'
+import { galleries } from '@/api/galleryApi'
 
 export default {
-  name: 'About',
+  name: 'Gallery',
   components: {
-    headerPhoto
+    headerPhoto,
+    Popup
+  },
+  data() {
+    return {
+      galleries: [],
+      showModel: false,
+      popupImage: ''
+    }
+  },
+  async created() {
+    await this.fetchGalleries()
+  },
+  methods: {
+    async fetchGalleries() {
+      try {
+        const response = await galleries.getAllGalleries()
+        this.galleries = response.data
+        console.log('Fetched galleries:', this.galleries)
+      } catch (error) {
+        console.error('Error fetching galleries:', error)
+      }
+    },
+    closeModal() {
+      this.showModel = false
+      this.showEdit = false
+    },
+    async handleGalleryDeleted(galleryId) {
+      this.galleries = this.galleries.filter(gallery => gallery._id !== galleryId)
+    },
+    async openPopup(image) {
+      this.popupImage = image
+      this.showModel = true
+    }
   }
 }
 </script>
+
+<style>
+.gallery-container {
+    display: block;
+    padding-top: 200px;
+    padding-bottom: 100px;
+}
+.gallery-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+.gallery-img {
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 20px;
+}
+.gallery-img >*{
+  flex: 1 0 0;
+  min-height: 0;
+  display: flex;
+}
+.gallery-img img {
+  max-height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0px 10px;
+  }
+
+</style>
